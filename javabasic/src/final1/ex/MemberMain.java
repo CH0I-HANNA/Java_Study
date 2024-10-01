f9671cee6a871cd71b69e725f894be5fe61655cb
Choi_Hanna <119294313+CH0I-HANNA@users.noreply.github.com>
2024-10-01 20:47:02 +0900

    Update MemberMain.java

--- javabasic/src/final1/ex/MemberMain.java
+++ javabasic/src/final1/ex/MemberMain.java
@@ -3,8 +3,6 @@ package final1.ex;
 public class MemberMain {
 
     public static void main(String[] args) {
-        Member member1 = new Member("hannachoe53", "최한나");
-        Member member2 = new Member("mids03", "최원삼");
         Member member1 = new Member("1234567", "최한나");
         Member member2 = new Member("12345679", "최둘셋");
 
