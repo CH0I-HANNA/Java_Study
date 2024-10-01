e59f0d1f41411f0e6274a0627b08cf27241a8103
Choi_Hanna <hannachoe53@gmail.com>
2024-10-01 22:53:41 +0900

    백준 풀기

---
+++ baekjoon/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        sc.nextLine();
+        int[] arr = new int[n];
+        int min;
+        int max;
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            arr[j] = sc.nextInt();
+        }
+
+        min = arr[i];
+        max = arr[i];
+
+        for (i = 0; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
