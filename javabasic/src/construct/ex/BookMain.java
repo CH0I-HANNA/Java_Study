e2102d6bbfb1cd68ec50d17f3a744faacc9e43f6
Choi_Hanna <hannachoe53@gmail.com>
2024-10-12 00:20:37 +0900

    ddd

--- javabasic/src/construct/ex/BookMain.java
+++ javabasic/src/construct/ex/BookMain.java
@@ -8,6 +8,7 @@ public class BookMain {
 
         Book book2 = new Book("hello hava", "seo");
         book2.displayInfo();
+        System.out.println("test");
 
         Book book3 = new Book("JPA 프로그래밍", "kim", 700);
         book3.displayInfo();
